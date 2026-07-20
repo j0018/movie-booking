babf3450067fbe45c9204401f40e682c1befa8b1
Maica <143175358+j0018@users.noreply.github.com>
2026-07-20 12:19:57 +0800

    Use constants for SMTP username and password
    Replaced hardcoded SMTP credentials with constants.

--- email_verify.php
+++ email_verify.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once 'config.local.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -16,8 +16,8 @@ function send_verification($fullname, $email, $OTP){
         $mail->isSMTP();                                      // Set mailer to use SMTP
         $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'reelwave.films@gmail.com';                 // SMTP username
-        $mail->Password = 'gswo falp lulo hmyv';                           // SMTP password
+        $mail->Username = SMTP_USER;
+        $mail->Password = SMTP_PASS;                           // SMTP password
         $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, ssl also accepted
         $mail->Port = 587;                                    // TCP port to connect to
     
